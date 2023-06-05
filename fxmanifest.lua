fe2d5ebf79bda3ab794ea2cf3402501f43144479
bitpredator <67551273+bitpredator@users.noreply.github.com>
2023-06-05 20:31:05 +0200

    chore: initial commit

---
+++ fxmanifest.lua
@@ -0,0 +1,66 @@
+fx_version 'adamant'
+
+game 'gta5'
+
+description 'ES Extended'
+
+lua54 'yes'
+version '0.0.4'
+
+shared_scripts {
+	'locale.lua',
+	'locales/*.lua',
+	'config.lua',
+	'config.weapons.lua',
+}
+
+server_scripts {
+	'@oxmysql/lib/MySQL.lua',
+	'server/common.lua',
+	'server/classes/player.lua',
+	'server/classes/overrides/*.lua',
+	'server/functions.lua',
+	'server/onesync.lua',
+	'server/paycheck.lua',
+	'server/main.lua',
+	'server/commands.lua',
+	'common/modules/math.lua',
+	'common/modules/table.lua',
+	'common/functions.lua'
+}
+
+client_scripts {
+	'client/common.lua',
+	'client/functions.lua',
+	'client/wrapper.lua',
+	'client/main.lua',
+	'client/modules/death.lua',
+	'client/modules/scaleform.lua',
+	'client/modules/streaming.lua',
+	'common/modules/math.lua',
+	'common/modules/table.lua',
+	'common/functions.lua'
+}
+
+ui_page {
+	'html/ui.html'
+}
+
+files {
+	'imports.lua',
+	'locale.js',
+	'html/ui.html',
+	'html/css/app.css',
+	'html/js/mustache.min.js',
+	'html/js/wrapper.js',
+	'html/js/app.js',
+	'html/fonts/pdown.ttf',
+	'html/fonts/bankgothic.ttf'
+}
+
+dependencies {
+	'/server:5949',
+	'/onesync',
+	'oxmysql',
+	'spawnmanager',
+}
