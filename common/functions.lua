5bbb7e90f443ae8f024bece67ca815b7790ded70
bitpredator <67551273+bitpredator@users.noreply.github.com>
2023-06-05 21:40:36 +0200

    chore: fix unused loop variable

--- common/functions.lua
+++ common/functions.lua
@@ -25,7 +25,7 @@ function ESX.GetWeapon(weaponName)
 end
 
 function ESX.GetWeaponFromHash(weaponHash)
-	for k,v in ipairs(Config.Weapons) do
+	for _,v in ipairs(Config.Weapons) do
 		if joaat(v.name) == weaponHash then
 			return v
 		end
@@ -39,7 +39,7 @@ end
 function ESX.GetWeaponLabel(weaponName)
 	weaponName = string.upper(weaponName)
 
-	for k,v in ipairs(Config.Weapons) do
+	for _,v in ipairs(Config.Weapons) do
 		if v.name == weaponName then
 			return v.label
 		end
@@ -50,9 +50,9 @@ function ESX.GetWeaponComponent(weaponName, weaponComponent)
 	weaponName = string.upper(weaponName)
 	local weapons = Config.Weapons
 
-	for k,v in ipairs(Config.Weapons) do
+	for _,v in ipairs(Config.Weapons) do
 		if v.name == weaponName then
-			for k2,v2 in ipairs(v.components) do
+			for _,v2 in ipairs(v.components) do
 				if v2.name == weaponComponent then
 					return v2
 				end
@@ -68,20 +68,20 @@ function ESX.DumpTable(table, nb)
 
 	if type(table) == 'table' then
 		local s = ''
-		for i = 1, nb + 1, 1 do
+		for _ = 1, nb + 1, 1 do
 			s = s .. "    "
 		end
 
 		s = '{\n'
 		for k,v in pairs(table) do
 			if type(k) ~= 'number' then k = '"'..k..'"' end
-			for i = 1, nb, 1 do
+			for _ = 1, nb, 1 do
 				s = s .. "    "
 			end
 			s = s .. '['..k..'] = ' .. ESX.DumpTable(v, nb + 1) .. ',\n'
 		end
 
-		for i = 1, nb, 1 do
+		for _ = 1, nb, 1 do
 			s = s .. "    "
 		end
 
